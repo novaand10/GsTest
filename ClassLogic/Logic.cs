883223af0c265263d1baf019dbeb1a5899b46a31
novaand10 <120262260+novaand10@users.noreply.github.com>
2022-12-11 09:19:28 +0700

    add project

---
+++ ClassLogic/Logic.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace ClassLogic
+{
+    public class Logic
+    {
+
+        public void GetFormula()
+        {
+            Console.WriteLine("On the 1st year she kills 1 villager");
+            Console.WriteLine("On the 2st year she kills 1 + 1 = " + Logic.GetNumOfKill(2) + " villager");
+            Console.WriteLine("On the 3st year she kills 1 + 1 + 2 = " + Logic.GetNumOfKill(3) + " villager");
+            Console.WriteLine("On the 4st year she kills 1 + 1 + 2 + 3= " + Logic.GetNumOfKill(4) + " villager");
+            Console.WriteLine("On the 5st year she kills 1 + 1 + 2 + 3 + 5 = " + Logic.GetNumOfKill(5) + " villager");
+            Console.WriteLine("and soon....");
+        }
+
+        public void GetAverrage(int a, int b, int c, int d)
+        {
+            Console.WriteLine("Person A Age of Death:" + a + ", Year of Death:" + b + ".\n");
+            Console.WriteLine("Person B Age of Death:" + c + ", Year of Death:" + d + ".\n");
+            Console.WriteLine("==================================================================================");
+            int selisiha = Logic.GetSelisih(a, b);
+            int selisihb = Logic.GetSelisih(c, d);
+            if (selisiha < 0 || selisihb < 0)
+            {
+                Console.Write("So the average is -1");
+            }
+            else
+            {
+                double nokA = Logic.GetNumOfKill(selisiha);
+                double nokb = Logic.GetNumOfKill(selisihb);
+                double avg = (nokb + nokA) / 2;
+                Console.WriteLine("Person A born on year = " + b + "-" + a + " = " + selisiha + " , number of people killed on year " + selisiha + " is " + nokA + "\n");
+                Console.WriteLine("Person B born on year = " + d + "-" + c + " = " + selisihb + ", number of people killed on year " + selisihb + " is " + nokb + "");
+
+                Console.WriteLine("==================================================================================");
+                Console.Write("So the average is (" + nokb + "+" + nokA + ") / 2 = ");
+                Console.Write(avg);
+                Console.WriteLine("\n==============================");
+            }
+        }
+        static int GetSelisih(int a, int b)
+        {
+            int c = b - a;
+            return c;
+        }
+
+        static double GetNumOfKill(int val)
+        {
+            int[] list = new int[3] { 0,1,1 };
+            int hasil = 1;
+            for (int i = 1; i < 100; i++)
+            {
+
+                //Console.WriteLine(hasil);
+                if (i == 1)
+                    {
+                        hasil = 1;
+                    }
+                else if (i == 2)
+                {
+                    hasil = 2;
+                }
+                else
+                    {
+                        list[0] = list[1];
+                        list[1] = list[2];
+                        list[2] = list[0] + list[1];
+
+                    hasil += list[2];
+                }
+
+
+                if ((val) == i)
+                {
+                    break;
+                }
+            }
+
+            //switch (val)
+            //{
+            //    case 1:
+            //        hasil = 1;
+            //        break;
+            //    case 2:
+            //        hasil = 2;
+            //        break;
+            //    case 3:
+            //        hasil = 5;
+            //        break;
+            //    case 4:
+            //        hasil = 7;
+            //        break;
+            //    case 5:
+            //        hasil = 12;
+            //        break;
+            //    case 6:
+            //        hasil = 20;
+            //        break;
+
+            //}
+
+            return hasil;
+        }
+    }
+}
---
+++ TestGS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassLogic;
+
+namespace TestGS
+{
+    class Program
+    {
+        static void Main()
+        {
+            ClassLogic.Logic _logic = new ClassLogic.Logic();
+            Console.WriteLine("\n");
+            Console.WriteLine("=== this Program to stop the killing by wild ===\n");
+            _logic.GetFormula();
+            Console.WriteLine("\n=============================================================\n");
+            Console.WriteLine("==== Input Data Person A & B ===");
+            Console.Write("\nPerson A Age of Death:");
+            int AOD = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Person A Year of Death:");
+            int YOD = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nPerson B Age of Death:");
+            int BOD = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Person B Year of Death:");
+            int BYOD = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=============================================================");
+            _logic.GetAverrage(AOD, YOD, BOD, BYOD);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
